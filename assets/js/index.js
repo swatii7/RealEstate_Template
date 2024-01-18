bd147e94e6af32113199ffe1ce138d27b6ece16a
swatii7 <swatic946@gmail.com>
2024-01-18 17:38:57 +0530

    setup project and complete navbar, hero section

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+// make navbar sticky
+var navbar = document.getElementById('navbar');
+var sticky = navbar.offsetTop;
+
+window.onscroll = function(){
+if(window.pageYOffset >= sticky){
+    navbar.classList.add('sticky')
+}
+else{
+    navbar.classList.remove('sticky');
+}
+}
