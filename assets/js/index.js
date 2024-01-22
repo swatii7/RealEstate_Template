e2551f8d7e09505cea95f45cd9bdd60cabb40d3b
swatii7 <swatic946@gmail.com>
2024-01-22 19:17:03 +0530

    pending js function of toggleclass

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,3 +15,20 @@ else{
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+// function to add class on click of slider
+var slides = document.querySelectorAll('.slide');
+var contents = document.querySelectorAll('.properties-content');
+
+
+function toggleActiveClass(clickedIndex) {
+    // Remove 'slide-active' class from all slides
+    slides.forEach(function (slide, index) {
+        if (index === clickedIndex) {
+            slide.classList.add('slide-active');
+        } else {
+            slide.classList.remove('slide-active');
+        }
+    });
+}
