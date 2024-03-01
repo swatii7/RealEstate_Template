151ca4709295676d5e932c80b91452237617d060
swatii7 <swatic946@gmail.com>
2024-03-01 18:19:57 +0530

    working on testimonials and cards

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,94 +1,92 @@
-// // make navbar sticky
-// var navbar = document.getElementById('navbar');
-// var sticky = navbar.offsetTop;
-// window.onscroll = function(){
-// if(window.pageYOffset >= sticky){
-//     navbar.classList.add('sticky')
-// }
-// else{
-//     navbar.classList.remove('sticky');
-// }
-// }
+// make navbar sticky
+var navbar = document.getElementById('navbar');
+var sticky = navbar.offsetTop;
+window.onscroll = function(){
+if(window.pageYOffset >= sticky){
+    navbar.classList.add('sticky')
+}
+else{
+    navbar.classList.remove('sticky');
+}
+}
 
-// // js of tooltips
+// js of tooltips
 
-// const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-// const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-
-
-// // function to add class on click of slider
-
-// function toggleItem(elem) {
-//     // Set the first slide and its properties-content as active by default
-//     elem[0].classList.add('slide-active');
-//     var firstContentElement = elem[0].querySelector('.properties-content');
-//     if (firstContentElement) {
-//         firstContentElement.classList.add('content-active');
-//     }
-
-//     for (var i = 0; i < elem.length; i++) {
-//         elem[i].addEventListener("click", function(e) {
-//             var current = this;
-//             for (var i = 0; i < elem.length; i++) {
-//                 if (current != elem[i]) {
-//                     elem[i].classList.remove('slide-active');
-//                     var contentElement = elem[i].querySelector('.properties-content');
-//                     if (contentElement) {
-//                         contentElement.classList.remove('content-active');
-//                     }
-//                 } else if (current.classList.contains('slide-active') === true) {
-//                     current.classList.remove('slide-active');
-//                     var contentElement = current.querySelector('.properties-content');
-//                     if (contentElement) {
-//                         contentElement.classList.remove('content-active');
-//                     }
-//                 } else {
-//                     current.classList.add('slide-active');
-//                     var contentElement = current.querySelector('.properties-content');
-//                     if (contentElement) {
-//                         contentElement.classList.add('content-active');
-//                     }
-//                 }
-//             }
-//             e.preventDefault();
-//         });
-//     }
-// }
-
-// toggleItem(document.querySelectorAll('.property-slide'));
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 
-// // carousel function
+// function to add class on click of slider
+function toggleItem(elem) {
+    for (var i = 0; i < elem.length; i++) {
+        elem[i].addEventListener("click", function(e) {
+            var current = this;
+            var contentElement = current.querySelector('.properties-content');
 
-// $(document).ready(function(){
-//     $('.swiper-slider_wrapper').slick({
-//         centerMode: true,
-//         centerPadding: '60px',
-//         slidesToShow: 3,
-//         slideToScroll: 6,
-//         dots: true,
-//         responsive: [
-//             {
-//                 breakpoint: 768,
-//                 settings: {
-//                     arrows: false,
-//                     centerMode: true,
-//                     centerPadding: '40px',
-//                     slidesToShow: 1
-//                 }
-//             },
-//             {
-//                 breakpoint: 480,
-//                 settings: {
-//                     arrows: false,
-//                     centerMode: true,
-//                     centerPadding: '40px',
-//                     slidesToShow: 1
-//                 }
-//             }
-//         ]
-//     });
-// });
+            if (current.classList.contains('slide-active')) {
+                current.classList.add('slide-active');
+                if (contentElement) {
+                    contentElement.classList.add('content-active');
+                }
+            } else {
+                // Remove active class from all slides
+                for (var j = 0; j < elem.length; j++) {
+                    elem[j].classList.remove('slide-active');
+                    var otherContentElement = elem[j].querySelector('.properties-content');
+                    if (otherContentElement) {
+                        otherContentElement.classList.remove('content-active');
+                    }
+                }
+                // Add active class to the clicked slide
+                current.classList.add('slide-active');
+                if (contentElement) {
+                    contentElement.classList.add('content-active');
+                }
+            }
+
+            e.preventDefault();
+        });
+    }
+}
+
+toggleItem(document.querySelectorAll('.property-slide'));
+
+
+// carousel function
+
+$(document).ready(function(){
+    $('.testimonial-slide_Wrapper').slick({
+        centerMode: true,
+        centerPadding: '450px',
+        slidesToShow: 1,
+        slideToScroll: 6,
+        dots: false,
+        arrows:false,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
+
 
 // For more information, see greensock.com/docs/v3/Plugins/ScrollTrigger
 //  gsap.registerPlugin(ScrollTrigger);
@@ -109,5 +107,5 @@ const tl = gsap.timeline({
   tl.to(".downArrow", {rotation: 180, duration: 3})
 
 
-  // gsap.to(".real_estate-img", {duration:5, x:-100})
-
+  gsap.fromTo(".real_estate-img", {x: -900, duration: 6}, {x: 0, duration: 6})
+  gsap.fromTo(".rental_estate-img", {x: 900, duration: 6}, {x: 0, duration: 6})
